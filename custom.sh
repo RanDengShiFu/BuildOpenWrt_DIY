c4a6f2f2f67ea63e78a349e52095b1404fd80691
WaGeLaiNa <91310698+WaGeLaiNa@users.noreply.github.com>
2021-09-24 16:06:47 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -3,3 +3,8 @@
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #修改路由器网关地址
 sed -i 's/192.168.1.1/192.168.222.1/g' package/base-files/files/bin/config_generate;
+
+#默认开启WiFi无线
+sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#配置无线加密方式和链接密码
+sed -i 's/encryption=none/encryption=psk2+ccmp\n                        set wireless.default_radio${devidx}.key=admin999+++/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
