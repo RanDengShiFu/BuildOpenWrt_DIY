f741f30c4f320acb66213955f599c6dd892c1118
WaGeLaiNa <91310698+WaGeLaiNa@users.noreply.github.com>
2021-09-24 15:51:13 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,5 +1,5 @@
 #Add Custom Feeds
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #修改路由器网关地址
 sed -i 's/192.168.1.1/192.168.222.1/g' package/base-files/files/bin/config_generate;
